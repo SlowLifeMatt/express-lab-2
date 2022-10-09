d9f2ed17f1544e6f106f0e65700d7c715b45d98c
Matthew Subin <slowlifematt@Matthews-MacBook-Pro.local>
2022-10-08 22:47:36 -0700

    Tip Calculator express application created.

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ app.get("/greeting/:name", (req, res) => {
     res.send(`"Whats up, ${(req.params.name)}! It's so great to see you!"`)
 })
 
+app.get("/tip/:total/:tipPercentage/", (req, res) => {
+    res.send(`${ parseInt(req.params.tipPercentage)  * parseInt(req.params.total) / 100} `)
+})
+
 app.listen(3000, () => {
     console.log(`Hello! I'm listening on port 3000...`)
 });
